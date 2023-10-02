9f4c6fa2ca236299743d6c648aa5be11363162ed
mikehwebdev <81847609+mikehwebdev@users.noreply.github.com>
2023-10-02 14:26:41 +0100

    Added JS

--- main.js
+++ main.js
@@ -1,5 +1,18 @@
+const mainText = document.querySelector('.main-text')
+const titleText = document.querySelector('.title')
 
-
-function test(){
-  console.log('test')
-}
+fetch('https://api.adviceslip.com/advice')
+    .then(res => {
+        if (res.status >= 200 && res.status <= 299) {
+            return res.json()
+        } else {
+            throw Error(res.status)
+        }
+    })
+    .then(data => {
+        titleText.innerHTML = `Advice #${data.slip.id}`
+        mainText.textContent = data.slip.advice
+    }).catch(error => {
+        titleText.innerHTML = `${error}`
+        mainText.innerHTML = `Something went wrong. <br> Please try again.`
+    })
